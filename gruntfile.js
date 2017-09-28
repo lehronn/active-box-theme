b410058abd233e67b79ff11003a23c0a459e81be
Mateusz Stomski <lehronn@gmail.com>
2017-09-28 10:51:05 +0200

    slider is works but animation is slickering

--- gruntfile.js
+++ gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
     sass: {
       options: {
         sourceMap: true,
-        outputStyle: 'compressed'
+        //outputStyle: 'compressed'
       },
       dev: {
         files: {
